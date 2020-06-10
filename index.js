da8e3b0c29a5b5f2ba7ed198218223c6dbd67260
Kévin Rossignol <kevin.rossignol37@yahoo.com>
2020-06-10 21:53:31 +0200

    essai

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-console.log('bonjour')
-fetch("http://localhost:3000/api/cameras").then(response =>{
+let articlesElt = document.getElementById("x");
+fetch("http://localhost:3000/api/teddies").then(response =>{
     return response.json();
 }).then(elements =>{
-    console.log(elements)
-})
-
-let img = document.createElement("img");
-img.src = "images/vcam_1.jpg";
-img.setAttribute("style", "width:30em", "height:30em");
-let img2 = document.createElement("img");
-img2.src = "images/vcam_2.jpg";
-img2.setAttribute("style", "width:30em", "height:30em");
-let img3 = document.createElement("img");
-img3.src = "images/vcam_3.jpg";
-img3.setAttribute("style", "width:30em", "height:30em");
-let img4 = document.createElement("img");
-img4.src = "images/vcam_4.jpg";
-img4.setAttribute("style", "width:30em", "height:30em");
-let img5 = document.createElement("img");
-img5.src = "images/vcam_5.jpg";
-img5.setAttribute("style", "width:30em", "height:30em");
-
-
-
-
-let div = document.getElementById("x");
-div.appendChild(img);
-let div2 = document.getElementById("x");
-div.appendChild(img2);
-let div3 = document.getElementById("x");
-div.appendChild(img3);
-let div4 = document.getElementById("x");
-div.appendChild(img4);
-let div5 = document.getElementById("x");
-div.appendChild(img5);
+    elements.forEach ( elements => {
+        let article = document.createElement("artcile")
+        let titreElt = document.createElement("h2");
+        titreElt.textContent = elements.name;
+        let imageElt = document.createElement("img");
+        imageElt.src = elements.imageUrl;
+        articlesElt.appendChild(article);
+        article.appendChild(titreElt);
+        article.appendChild(imageElt);
+        
+    });
+   function versPageDeux (){
+       document.location.href="G:/open/orinoco/front_end/produit.html"
+   }
+});
 
--- main.js
+++
@@ -1,1 +0,0 @@
-fichierProduit.ficheProduit();
