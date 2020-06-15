ddcec04cf8a9f1bfbea818b7efe3d4ee1dcb5635
Kévin Rossignol <kevin.rossignol37@yahoo.com>
2020-06-15 18:05:20 +0200

    essaie

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ fetch("http://localhost:3000/api/teddies").then(response =>{
     return response.json();
 }).then(elements =>{
     elements.forEach ( elements => {
-        let article = document.createElement("artcile")
+        let article = document.createElement("article");
         let titreElt = document.createElement("h2");
         titreElt.textContent = elements.name;
         let imageElt = document.createElement("img");
         imageElt.src = elements.imageUrl;
+        let idElt = document.createElement("id");
+        idElt.textContent = elements._id;
+        
         articlesElt.appendChild(article);
         article.appendChild(titreElt);
         article.appendChild(imageElt);
-        
+        article.appendChild(idElt);
+        let lien = document.getElementById("lien");
+        article.appendChild(lien);
+    
     });
-   function versPageDeux (){
-       document.location.href="G:/open/orinoco/front_end/produit.html"
-   }
+    
 });
 
--- produit.js
+++ produit.js
@@ -1,13 +1,41 @@
-class fichierProduit {
+let articlesElt = document.getElementById("produit");
+fetch("http://localhost:3000/api/teddies").then(response =>{
+    return response.json();
+}).then(elements =>{
+    elements.forEach ( elements => {
+        let id = elements._id;
+        if (id = true){
+                let article = document.createElement('article');
+                let nom = document.createElement('h2');
+                nom.textContent = elements.name;
+                let image = document.createElement('img');
+                image.src = elements.imageUrl;
+                let description = document.createElement('p');
+                description.textContent = elements.description;
+                let couleur = document.createElement('button');
+                couleur.textContent = "Choisir la couleur";
+                let prix = document.createElement('p');
+                prix.textContent = elements.price;
+        
+            articlesElt.appendChild(article);
+            article.appendChild(nom);
+            article.appendChild(image);
+            article.appendChild(description);
+            article.appendChild(couleur);
+            article.appendChild(prix);
+            
+            couleur.setAttribute('id', "couleurs");
+            let couleurs = document.getElementById('couleurs').addEventListener('click', (elements) => {
+                elements.colors;
+            })
+            
+            
+            }else{
+                return false;
+        }
+            })
+    
+    
+});
 
 
-    ficheProduit() {
-        document.getElementById("x").addEventListener("click", () => {
-            cameraSchema.setName('name');
-            cameraSchema.setPrice("price");
-            cameraSchema.setDescription("Description");
-            cameraSchema.setLenses("Lenses");
-        })
-    }
-
-}
